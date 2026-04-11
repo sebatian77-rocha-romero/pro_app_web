3379c5d55f72e1500568d4281a350ef019e81494
Sebas @ <jesus_3041240463@utd.edu.mx>
2026-04-11 17:39:43 -0600

    cambios para conectar bd con las variables de entorno

--- backend/bd.js
+++ backend/bd.js
@@ -1,6 +1,27 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "inventario",
+    port: process.env.DB_PORT || 3306
+});
+
+db.connect((err) => {
+    if(err) {
+        console.error('Error de conexion: ', err)
+        return;
+    }
+    console.log("conexion exitosa a la base de datos")
+});
+
+module.exports = db;
+
+/*
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
@@ -18,3 +39,4 @@ db.connect((err) => {
 });
 
 module.exports = db;
+*/
